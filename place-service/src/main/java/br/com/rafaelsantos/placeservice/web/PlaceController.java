9b094aa7990812905fa566f0c6243e05bbee3112
Rafael Santos <rafa.05125@gmail.com>
2023-06-28 16:57:24 -0300

    Validation

--- place-service/src/main/java/br/com/rafaelsantos/placeservice/web/PlaceController.java
+++ place-service/src/main/java/br/com/rafaelsantos/placeservice/web/PlaceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.rafaelsantos.placeservice.api.PlaceRequest;
 import br.com.rafaelsantos.placeservice.api.PlaceResponse;
 import br.com.rafaelsantos.placeservice.domain.PlaceService;
+import jakarta.validation.Valid;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,7 +24,7 @@ public class PlaceController {
     }
 
     @PostMapping
-    public ResponseEntity<Mono<PlaceResponse>> create(@RequestBody PlaceRequest request) {
+    public ResponseEntity<Mono<PlaceResponse>> create(@Valid @RequestBody PlaceRequest request) {
         var placeResponse = placeService.create(request).map(PlaceMapper::fromPlaceToResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(placeResponse);
     }
