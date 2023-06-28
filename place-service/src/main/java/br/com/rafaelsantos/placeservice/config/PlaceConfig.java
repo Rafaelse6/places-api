c8fc67568261fe8f9987a8507505ea4091d99a9c
Rafael Santos <rafa.05125@gmail.com>
2023-06-28 16:38:23 -0300

    Audit config

--- place-service/src/main/java/br/com/rafaelsantos/placeservice/config/PlaceConfig.java
+++ place-service/src/main/java/br/com/rafaelsantos/placeservice/config/PlaceConfig.java
@@ -2,11 +2,13 @@ package br.com.rafaelsantos.placeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 import br.com.rafaelsantos.placeservice.domain.PlaceRepository;
 import br.com.rafaelsantos.placeservice.domain.PlaceService;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
 
     @Bean
